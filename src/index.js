80a82c167cf78173cc84395227403ce91838a9cb
Patrick Watrous <patrickwatrous@live.com>
2018-01-11 12:10:41 -0500

    addeda  working reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const reducer = (state={}, action) => {
+	switch (action.type) {
+		case "TOGGLE_GRAPH":
+			return { ...state, selectedData: action.payload };
+		default:
+			return state;
+	}
+};
+
+const store = createStore(reducer);
